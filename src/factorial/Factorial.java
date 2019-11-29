023a6f499ae1be33c39645dfe97d0824d10e9751
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2019-11-29 13:55:29 +0100

    factorial refactorizado

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -4,22 +4,22 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        int j;
-        int f;
+        int numero;
+        int factorial;
 
-        j = 8;
+        numero = 8;
 
         int i;
-        if (j == 0) {
-            f = 1;
+        if (numero == 0) {
+            factorial = 1;
         } else {
-            f = 1;
-            for (i = j; i >= 1; i--) {
-                f = f * i;
+            factorial = 1;
+            for (i = numero; i >= 1; i--) {
+                factorial = factorial * i;
             }
         }
 
-        System.out.println(f);
+        System.out.println(factorial);
 
     }
 
